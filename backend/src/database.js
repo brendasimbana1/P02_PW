5319e14a990364b91f3b38217d8aa21b65552d27
brendasimbana1 <bsimbanag1@est.ups.edu.ec>
2024-07-23 21:33:32 -0500

    Mongo Atlas bc

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://bsimbanag1:1234@cluster0.u1z20lj.mongodb.net/gastos')
+mongoose.connect('mongodb+srv://bsimbanag1:1234@cluster0.u1z20lj.mongodb.net/gastos?retryWrites=true&w=majority')
 
 .then(db => console.log('Database is connected'))
 .catch(err => console.log('err'))
